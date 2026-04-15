a1b7147f7d89547d550246c18366117f617527bc
Pekka Tapio Aalto <pekka.aalto@sasky.fi>
2026-04-15 12:17:11 +0300

    lisää Items-komponentin

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,7 @@
 import { ButtonContainer, FloatingButton } from '../../shared/ui/buttons'
 import Content from '../Content'
 import Header from '../Header'
-import Item from '../Item'
+import Items from '../../pages/Items'
 import Menu from '../Menu'
 import styles from './App.module.scss'
 
@@ -13,11 +13,7 @@ function App() {
       <div className={styles.app}>
         <Header />
         <Content>
-          <Item />
-          <Item />
-          <Item />
-          <Item />
-          <FloatingButton secondary>+</FloatingButton>
+          <Items />
         </Content>
         <Menu />
       </div>
