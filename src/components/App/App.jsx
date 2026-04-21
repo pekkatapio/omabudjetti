b541bfc07007028520e7ff588e5f78558c1a9540
Pekka Tapio Aalto <pekka.aalto@sasky.fi>
2026-04-21 14:07:31 +0300

    lisää AppRouter-komponentin ja reitityksen

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,28 +1,12 @@
-import { ButtonContainer, FloatingButton } from '../../shared/ui/buttons'
-import Content from '../Content'
-import Header from '../Header'
-import Items from '../../pages/Items'
-import Menu from '../Menu'
-import Settings from '../../pages/Settings'
-import Stats from '../../pages/Stats'
-import styles from './App.module.scss'
+import AppRouter from '../../router/AppRouter'
 
 function App() {
 
   return (
     <>
-      <ButtonContainer>
-      <div className={styles.app}>
-        <Header />
-        <Content>
-          <Settings />
-        </Content>
-        <Menu />
-      </div>
-      </ButtonContainer>
+      <AppRouter />
     </>
   )
-
 }
 
 export default App
