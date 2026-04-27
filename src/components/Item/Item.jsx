72b7d2303100039dfa45059480762b90cca932ba
Pekka Tapio Aalto <Pekka.Aalto@sasky.fi>
2026-04-27 09:51:05 +0300

    lisää valuuttamuotoilun Item-komponentille

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -10,11 +10,20 @@ function Item({data, ...props}) {
   // lokaalin mukaan.
   const paymentDate = new Date(data.paymentDate).toLocaleDateString(locale)
 
+  // Luodaan NumberFormat-olio euromääräisten summien esittämiseen.
+  const numberFormat = new Intl.NumberFormat(locale, {
+    style: 'currency',
+    currency: 'EUR'
+  })
+
+  // Muotoillaan rahasumma käyttäjän lokaalin mukaiseen valuuttamuotoon.
+  const amount = numberFormat.format(data.amount)
+
   return (
     <div className={styles.item}>
       <div className={styles.item_data}>
         <div className={styles.item_type}>{data.type}</div>
-        <div className={styles.item_amount}>{data.amount} €</div>
+        <div className={styles.item_amount}>{amount}</div>
         <div className={styles.item_date}>{paymentDate}</div>
         <div className={styles.item_timespan}>{data.periodStart} - {data.periodEnd}</div>
         <div className={styles.item_receiver}>{data.receiver}</div>
