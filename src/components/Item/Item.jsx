bae8bc8a442aa46c102f558b5616c3771da9e766
Pekka Tapio Aalto <Pekka.Aalto@sasky.fi>
2026-04-27 09:43:25 +0300

    lisää päivämäärämuotoilun Item-komponentille

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -3,12 +3,19 @@ import styles from './Item.module.scss'
 
 function Item({data, ...props}) {
 
+  // Käytettävä lokaaliasetus päivämäärän muotoiluun.
+  const locale = "fi-FI"
+
+  // Muodostetaan maksupäivä Date-oliosta ja muotoillaan se käyttäjän
+  // lokaalin mukaan.
+  const paymentDate = new Date(data.paymentDate).toLocaleDateString(locale)
+
   return (
     <div className={styles.item}>
       <div className={styles.item_data}>
         <div className={styles.item_type}>{data.type}</div>
         <div className={styles.item_amount}>{data.amount} €</div>
-        <div className={styles.item_date}>{data.paymentDate}</div>
+        <div className={styles.item_date}>{paymentDate}</div>
         <div className={styles.item_timespan}>{data.periodStart} - {data.periodEnd}</div>
         <div className={styles.item_receiver}>{data.receiver}</div>
         <div className={styles.item_average}>? €/kk</div>
