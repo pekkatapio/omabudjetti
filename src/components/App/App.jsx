f1da5fb0bec07893e4b7a4494d3f081b6a3eb7fb
Pekka Tapio Aalto <pekka.aalto@sasky.fi>
2026-04-28 11:02:04 +0300

    lisää lomaketietojen tallennuksen

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,28 @@
 import AppRouter from '../../router/AppRouter'
+import { useState } from 'react'
 import testdata from './testdata.js'
 
 function App() {
 
+  // Sovelluksen merkintädata, joka välitetään eteenpäin reitittäjälle.
+  const [data, setData] = useState(testdata)
+
+  // Käsittelee ja tallentaa lomakkeelle syötetyistä
+  // tiedoista uuden rivin tai muokkaa olemassaolevaa.
+  const handleItemSubmit = (newitem) => {
+    let copy = data.slice()
+    copy.push(newitem)
+    copy.sort( (a,b) => {
+      const aDate = new Date(a.paymentDate)
+      const bDate = new Date(b.paymentDate)
+      return bDate - aDate
+    })
+    setData(copy)
+  }
+
   return (
     <>
-      <AppRouter data={testdata} />
+      <AppRouter data={data} onItemSubmit={handleItemSubmit} />
     </>
   )
 }
