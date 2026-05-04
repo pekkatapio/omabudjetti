2396cc2eac1165e08a5cf99089519ef62575e74e
Pekka Tapio Aalto <pekka.aalto@sasky.fi>
2026-05-04 08:31:23 +0300

    lisää kulutyyppilistan tulostuksen

--- src/pages/Settings/Settings.jsx
+++ src/pages/Settings/Settings.jsx
@@ -1,12 +1,19 @@
 import styles from './Settings.module.scss'
 
-function Settings() {
+function Settings(props) {
 
   return (
     <div className={styles.settings}>
-      <h2>Settings</h2>
+      <h2>Asetukset</h2>
+      <h3>Kulutyypit</h3>
+      <div className={styles.settings_types}>
+        { props.typelist.map(
+            type => <div key={type}>{type}</div>
+        )}
+      </div>
     </div>
   )
+
 }
 
 export default Settings
