15cefdfb7066b7d0173b31e86821107b821a73a5
Pekka Tapio Aalto <pekka.aalto@sasky.fi>
2026-05-05 13:34:16 +0300

    lisää kulutyyppien lisäämisen Firestore-tietokantaan

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,7 +2,7 @@ import AppRouter from '../../router/AppRouter'
 import { useState } from 'react'
 import useLocalStorage from '../../shared/hooks/uselocalstorage'
 import firebase from './firebase.js'
-import { collection, deleteDoc, doc, getFirestore, onSnapshot, orderBy, query, setDoc  } from 'firebase/firestore'
+import { addDoc, collection, deleteDoc, doc, getFirestore, onSnapshot, orderBy, query, setDoc  } from 'firebase/firestore'
 import { useEffect } from 'react'
 
 function App() {
@@ -11,7 +11,7 @@ function App() {
   const [data, setData] = useState([])
 
   // Sovelluksen kulutyypit, jotka välitetään eteenpäin reitittäjälle.
-  const [typelist, setTypelist] = useLocalStorage('omabudjetti-typelist',[])
+  const [typelist, setTypelist] = useState([])
 
   // Alustetaan Firestore-tietokantayhteys annetulla Firebase-sovelluksella.
   const firestore = getFirestore(firebase)
@@ -33,6 +33,22 @@ function App() {
     return unsubscribe
   }, [])
 
+  // useEffect-kuuntelija, joka hakee Firestoresta type-kokoelman
+  // type-kentät aakkosjärjestyksessä ja päivittää ne typelist-tilaan
+  // reaaliaikaisesti.
+  useEffect( () => {
+    const unsubscribe = onSnapshot(query(collection(firestore,'type'),
+                                         orderBy('type')),
+                                   snapshot => {
+      const newTypelist = []
+      snapshot.forEach( doc => {
+        newTypelist.push(doc.data().type)
+      })
+      setTypelist(newTypelist)
+    })
+    return unsubscribe
+  }, [])
+
   // Poistaa olemassa olevan tuotteen Firestore-tietokannasta
   // item-kokoelmasta annetun dokumentin id-tunnisteen perusteella.
   const handleItemDelete = async (id) => {
@@ -46,14 +62,9 @@ function App() {
     await setDoc(doc(firestore, 'item', newitem.id), newitem)
   }
 
-  // Käsittelee uuden tyypin lisäyksen, lisää annetun
-  // type-arvon typelist-taulukkoon, järjestää listan
-  // ja päivittää staten.
-  const handleTypeSubmit = (type) => {
-    let copy = typelist.slice()
-    copy.push(type)
-    copy.sort()
-    setTypelist(copy)
+  // Tallentaa uuden tyypin Firestore-tietokannan type-kokoelmaan.
+  const handleTypeSubmit = async (type) => {
+    await addDoc(collection(firestore,'type'),{type: type})
   }
 
   return (
